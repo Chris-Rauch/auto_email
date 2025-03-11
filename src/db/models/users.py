3acd734e856e8d6c8d6e957de02096b75c116f8f
Chris-Rauch <rauch.christopher13@gmail.com>
2025-03-11 15:05:44 -0700

    Commiting for re-merge

--- src/db/models/users.py
+++ src/db/models/users.py
@@ -8,9 +8,10 @@ class UserBase(SQLModel):
     last_name: str | None = Field(default=None, nullable=True, max_length=255)
 
 class User(UserBase, table=True):
+    __tablename__ = "users" #override table name to match mySQL table name
     user_id: int | None = Field(default=None, primary_key=True, index=True, nullable=False)
     pass_hash: str = Field(nullable=False, max_length=255)
-    created_at: datetime = Field(default_factory=datetime.timezone.utc, nullable=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False) # TODO utcnow is deprecated. Change for future use
 
 class UserPublic(UserBase):
     user_id: int
